0c88a4f313b54022ce51c2fc48bc31c6ea10720a
Asier <alumno@gmail.com>
2025-02-20 20:40:03 +0100

    Instalación de Taildwindcss

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
 
   <MeusNumerator :valorem="7"/>
   <MeusNumeratorScript :valorem="10"/>
+  <MeusNumeratorScript2 :valorem="15"/>
 </template>
 
 <script lang="ts" setup>
+import MeusNumeratorScript2 from './components/meus-numerator-script/MeusNumeratorScript2.vue';
 import MeusNumerator from './components/MeusNumerator.vue';
 import MeusNumeratorScript from './components/MeusNumeratorScript.vue';
 
