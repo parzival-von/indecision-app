6405faea4c91f97467699fd499c847c370349ffc
Asier <alumno@gmail.com>
2025-02-20 19:53:50 +0100

    Diferentes modos para crear componentes

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <h1>Mi primera App de Vue</h1>
 
   <MeusNumerator :valorem="7"/>
+  <MeusNumeratorScript :valorem="10"/>
 </template>
 
 <script lang="ts" setup>
 import MeusNumerator from './components/MeusNumerator.vue';
+import MeusNumeratorScript from './components/MeusNumeratorScript.vue';
 
 
 </script>
---
+++ src/components/MeusNumeratorScript.vue
@@ -0,0 +1,28 @@
+<template>
+  <section>
+    <h3>Counter:{{ numerus }}</h3>
+    <h3>Square: {{ quadrataNumerus }}</h3>
+
+    <div>
+      <button @:click="numerus++">+1</button>
+      <button @:click="numerus--">-1</button>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { computed, ref, defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    valorem: { type: Number, required: true },
+  },
+  setup(props) {
+    const numerus = ref(props.valorem)
+    const quadrataNumerus = computed(() => numerus.value * numerus.value)
+    return {
+      numerus,
+      quadrataNumerus,
+    }
+  },
+})
+</script>
