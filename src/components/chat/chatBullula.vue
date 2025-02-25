4d1c24b11f3718b2eadd3ebf280c2eafecbadd42
Asier <alumno@gmail.com>
2025-02-25 20:40:31 +0100

    api yesno

---
+++ src/components/chat/chatBullula.vue
@@ -0,0 +1,27 @@
+<template>
+    <div v-if="meusEst" class="flex justify-end">
+        <!-- div burbujas -->
+        <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">
+            {{ nuntius }}
+        </div>
+      </div>
+
+      <div v-else class="flex justify-start">
+        <!-- div burbujas -->
+        <div class="bg-gray-300 text-black p-2 max-w-xs rounded-lg">
+          
+          <span class="capitalize">{{nuntius}}</span>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+interface Props{
+    nuntius: string;
+    meusEst: boolean;
+    imago?: string;
+}
+
+defineProps<Props>();
+
+</script>
--- src/components/chat/chatNuntii.vue
+++ src/components/chat/chatNuntii.vue
@@ -1,21 +1,13 @@
 <template>
-    
-  <div class="p-4 flex-1 overflow-y-auto">
-    <!-- div que contiene los mensajes -->
-    <div class="flex flex-col space-y-2">
-      <!-- div que contiene las burbujas-->
-      <div class="flex justify-end">
-        <!-- div burbujas -->
-        <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">hola, ¿Cómo te va el dia?</div>
-      </div>
 
-      <div class="flex justify-start">
-        <!-- div burbujas -->
-        <div class="bg-gray-300 text-black p-2 max-w-xs rounded-lg">
-          Muy bien, estoy en casa. ¿Y yú?
-        </div>
-      </div>
-    </div>
+  <div class="p-4 flex-1 overflow-y-auto"><!-- div que contiene los mensajes -->
+    <ChatBullula :meus-est="true" nuntius="¿salimos luego a dar una vuelta?"/>
   </div>
 
 </template>
+
+<script lang="ts" setup>
+import ChatBullula from './chatBullula.vue';
+
+
+</script>
