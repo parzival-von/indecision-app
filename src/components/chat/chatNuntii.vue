d38cac07dfe379b7a15500c426b9fa37ad187761
Asier <alumno@gmail.com>
2025-02-25 20:18:39 +0100

    Componentes del chat

---
+++ src/components/chat/chatNuntii.vue
@@ -0,0 +1,21 @@
+<template>
+    
+  <div class="p-4 flex-1 overflow-y-auto">
+    <!-- div que contiene los mensajes -->
+    <div class="flex flex-col space-y-2">
+      <!-- div que contiene las burbujas-->
+      <div class="flex justify-end">
+        <!-- div burbujas -->
+        <div class="bg-blue-200 text-black p-2 max-w-xs rounded-lg">hola, ¿Cómo te va el dia?</div>
+      </div>
+
+      <div class="flex justify-start">
+        <!-- div burbujas -->
+        <div class="bg-gray-300 text-black p-2 max-w-xs rounded-lg">
+          Muy bien, estoy en casa. ¿Y yú?
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
