935e8bdfe43d4d437d4fb52ff5017549106adc74
Asier <alumno@gmail.com>
2025-02-27 19:07:51 +0100

    YesNo Gif

--- src/components/chat/chatNuntii.vue
+++ src/components/chat/chatNuntii.vue
@@ -1,13 +1,18 @@
+
 <template>
 
-  <div class="p-4 flex-1 overflow-y-auto"><!-- div que contiene los mensajes -->
-    <ChatBullula :meus-est="true" nuntius="¿salimos luego a dar una vuelta?"/>
-  </div>
+  <div class="p-4 flex-1 overflow-y-auto"> <!-- div que contiene los mensajes -->
+    <div class="flex flex-col space-y-2">
 
+      <ChatBullula :meus-est="true" nuntius="¿salimos luego a dar una vuelta?" />
+      <ChatBullula 
+      :meus-est="false"
+      nuntius="no"
+      imago="https://yesno.wtf/assets/yes/13-c3082a998e7758be8e582276f35d1336.gif"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import ChatBullula from './chatBullula.vue';
-
-
+import ChatBullula from './chatBullula.vue'
 </script>
